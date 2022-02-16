1ca7abc4a330718a08d9f6391cfd307dd1be9376
Alex RT <alex_er@hotmail.com>
2022-02-15 20:05:19 -0500

    ordenando 2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import Layout from "./components/layout/Layout";
 //Css y boxicon
 import './assets/boxicons-2.0.7/css/boxicons.min.css'
 import './assets/css/grid.css'
-import './assets/css/theme.css'
 import './assets/css/index.css'
 
 document.title = "AGP"
