a4728609563773a6a81476ae9d0f6d01f718bf6c
Alex Ramos <aletsoshi@outlook.com>
2022-05-08 17:56:35 -0500

    style: :art: Projects table as function

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,71 +1,77 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
-import ViewKanbanIcon from "@mui/icons-material/ViewKanban";
 import DownloadForOfflineIcon from "@mui/icons-material/DownloadForOffline";
+//import ViewKanbanIcon from "@mui/icons-material/ViewKanban";
 import swal from "sweetalert";
 import { GridActionsCellItem, DataGrid } from "@mui/x-data-grid";
 import fileDownload from "js-file-download";
+import { useHistory } from "react-router-dom";
 
-class TableProjects extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { projects: [] };
-  }
+const TableProjects = () => {
+  const history = useHistory();
+  const [dataProject, setDataProject] = useState([]);
+  const url = process.env.REACT_APP_URL;
 
-  componentDidMount() {
-    //Projects
-    axios.get(process.env.REACT_APP_URL + "/projects").then((res) => {
-      this.setState({ projects: res.data.data });
+  //Projects
+  useEffect(() => {
+    axios.get(url + "/projects").then((res) => {
+      setDataProject(res.data.data);
     });
-  }
+  }, [url, setDataProject]);
 
-  //Kanban
-  kanban(id) {
+  //Kanban(a futuro)
+  function kanban(id) {
     if (id > 0) {
-      return (window.location.href = "/gant/" + id);
+      history.push("/gant/" + id);
     } else {
       swal("Error", "Hay un error con la fila", "error");
     }
   }
 
   //Descarga
-  descargar(id) {
-    let data = this.state.projects;
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].id === id) {
-        const archivoURL = data[i].archivo;
-        //en replace([\\/ le quite un / por recomendacion de eslint])
-        const filename = archivoURL.replace(/^.*[\\/]/, "");
-        let fileExtension = archivoURL.split(".");
-        fileExtension = fileExtension[fileExtension.length - 1];
-        axios({
-          url: process.env.REACT_APP_URL + "/descarga/" + filename,
-          method: "get",
-          responseType: "blob",
-        }).then((res) => {
-          fileDownload(res.data, `${data[i].nombre}.${fileExtension}`);
-        });
+  function descargar(id) {
+    for (let i = 0; i < dataProject.length; i++) {
+      if (dataProject[i].id === id) {
+        const archivoURL = dataProject[i].archivo;
+        if (archivoURL) {
+          //en replace([\\/ le quite un / por recomendacion de eslint])
+          const filename = archivoURL.replace(/^.*[\\/]/, "");
+          let ext = archivoURL.split(".");
+          ext = ext[ext.length - 1];
+          axios({
+            url: url + "/descarga/" + filename,
+            method: "get",
+            responseType: "blob",
+          }).then((res) => {
+            fileDownload(res.dataProject, `${dataProject[i].nombre}.${ext}`);
+          });
+          swal("Se descargo el archivo exitosamente!", {
+            icon: "success",
+          });
+        } else {
+          swal("Error", "Este proyecto no contiene un archivo", "error");
+        }
       }
     }
   }
 
   //Editar
-  editlink(id, modelo) {
+  function editlink(id, modelo) {
     if (modelo === "Proyecto") {
-      return (window.location.href = "/proyectos/" + id);
+      history.push("/proyectos/" + id);
     } else if (modelo === "Innovacion") {
-      window.location.href = "http://facebook.com";
+      history.push("/innovaciones/" + id);
     } else if (modelo === "Proceso") {
-      window.location.href = "http://twitter.com";
+      history.push("/procesos/" + id);
     } else {
       swal("Error", "Hay un error con la fila", "error");
     }
   }
 
   //DELETE
-  eliminar(id) {
+  function eliminar(id) {
     swal({
       title: "¿Esta seguro de eliminar la fila seleccionada?",
       text: "Esta acción no se puede deshacer.",
@@ -74,151 +80,142 @@ class TableProjects extends Component {
       dangerMode: true,
     }).then((value) => {
       if (value) {
-        axios
-          .delete(process.env.REACT_APP_URL + "/projects/" + id)
-          .then((res) => {
-            var temp = this.state.projects.filter(
-              (project) => project.id !== id
-            );
-            this.setState({
-              projects: temp,
-            });
-            swal("Se elimino la fila exitosamente!", {
-              icon: "success",
-            });
+        axios.delete(url + "/projects/" + id).then((res) => {
+          var temp = dataProject.filter((project) => project.id !== id);
+          //Actualiza la tabla automaticamente
+          setDataProject(temp);
+          swal("Se elimino la fila exitosamente!", {
+            icon: "success",
           });
+        });
       } else {
         swal("Se cancelo la eliminación!");
       }
     });
   }
 
-  render() {
-    return (
-      <>
-        <h2 className="page-header">Tabla General</h2>
-        <div className="card" style={{ height: 762, width: "100%" }}>
-          <DataGrid
-            getRowId={(row) => row.id}
-            rows={this.state.projects}
-            columns={[
-              {
-                field: "modelo",
-                headerName: "MODELO",
-                width: 100,
+  return (
+    <>
+      <h2 className="page-header">Tabla General</h2>
+      <div className="card" style={{ height: 762, width: "100%" }}>
+        <DataGrid
+          getRowId={(row) => row.id}
+          rows={dataProject}
+          columns={[
+            {
+              field: "modelo",
+              headerName: "MODELO",
+              width: 100,
+            },
+            { field: "estado", headerName: "ESTADO", width: 130 },
+            { field: "tipo", headerName: "TIPO", width: 100 },
+            { field: "nombre", headerName: "NOMBRE", width: 200 },
+            {
+              field: "responsable",
+              headerName: "RESPONSABLE",
+              width: 150,
+              valueGetter(params) {
+                return `${params.row.responsable.nombre}`;
               },
-              { field: "estado", headerName: "ESTADO", width: 130 },
-              { field: "tipo", headerName: "TIPO", width: 100 },
-              { field: "nombre", headerName: "NOMBRE", width: 200 },
-              {
-                field: "responsable",
-                headerName: "RESPONSABLE",
-                width: 150,
-                valueGetter(params) {
-                  return `${params.row.responsable.nombre}`;
-                },
+            },
+            {
+              field: "areas",
+              headerName: "AREA (USUARIA - RESPONSABLE)",
+              width: 300,
+              valueGetter(params) {
+                return `${params.row.area_usuario || "No hay"}\n - ${
+                  params.row.area_responsable || "No hay"
+                }`;
               },
-              {
-                field: "areas",
-                headerName: "AREA (USUARIA - RESPONSABLE)",
-                width: 300,
-                valueGetter(params) {
-                  return `${params.row.area_usuario || "No hay"}\n - ${
-                    params.row.area_responsable || "No hay"
-                  }`;
-                },
+            },
+            {
+              field: "empresa_usuario",
+              headerName: "EMPRESA USUARIO",
+              width: 150,
+            },
+            {
+              field: "empresa_responsable",
+              headerName: "EMPRESA USUARIO",
+              width: 150,
+            },
+            { field: "sede_usuario", headerName: "SEDE USUARIO", width: 130 },
+            {
+              field: "sede_responsable",
+              headerName: "SEDE RESPONSABLE",
+              width: 150,
+            },
+            { field: "prioridad", headerName: "PRIORIDAD", width: 100 },
+            {
+              field: "responsabilidad",
+              headerName: "RESPONSABILIDAD",
+              width: 150,
+            },
+            {
+              field: "costo",
+              headerName: "COSTO",
+              type: "number",
+              width: 70,
+            },
+            {
+              field: "fecha_identificacion",
+              headerName: "FECHA IDENTIF.",
+              width: 130,
+              valueGetter(params) {
+                return params.row.fecha_identificacion.split("T")[0];
               },
-              {
-                field: "empresa_usuario",
-                headerName: "EMPRESA USUARIO",
-                width: 150,
+            },
+            {
+              field: "fecha_inicio",
+              headerName: "FECHA INICIO",
+              width: 130,
+              valueGetter(params) {
+                return params.row.fecha_inicio.split("T")[0];
               },
-              {
-                field: "empresa_responsable",
-                headerName: "EMPRESA USUARIO",
-                width: 150,
+            },
+            {
+              field: "fecha_cierre",
+              headerName: "FECHA CIERRE",
+              width: 130,
+              type: "date",
+              valueGetter(params) {
+                return params.row.fecha_cierre.split("T")[0];
               },
-              { field: "sede_usuario", headerName: "SEDE USUARIO", width: 130 },
-              {
-                field: "sede_responsable",
-                headerName: "SEDE RESPONSABLE",
-                width: 150,
-              },
-              { field: "prioridad", headerName: "PRIORIDAD", width: 100 },
-              {
-                field: "responsabilidad",
-                headerName: "RESPONSABILIDAD",
-                width: 150,
-              },
-              {
-                field: "costo",
-                headerName: "COSTO",
-                type: "number",
-                width: 70,
-              },
-              {
-                field: "fecha_identificacion",
-                headerName: "FECHA IDENTIF.",
-                width: 130,
-                valueGetter(params) {
-                  return params.row.fecha_identificacion.split("T")[0];
-                },
-              },
-              {
-                field: "fecha_inicio",
-                headerName: "FECHA INICIO",
-                width: 130,
-                valueGetter(params) {
-                  return params.row.fecha_inicio.split("T")[0];
-                },
-              },
-              {
-                field: "fecha_cierre",
-                headerName: "FECHA CIERRE",
-                width: 130,
-                type: "date",
-                valueGetter(params) {
-                  return params.row.fecha_cierre.split("T")[0];
-                },
-              },
-              {
-                field: "actions",
-                headerName: "OPCIONES",
-                type: "actions",
-                width: 150,
-                getActions: (params) => [
-                  <GridActionsCellItem
-                    icon={<DownloadForOfflineIcon />}
-                    label="Descargar"
-                    onClick={() => this.descargar(params.row.id)}
-                  />,
-                  <GridActionsCellItem
+            },
+            {
+              field: "actions",
+              headerName: "OPCIONES",
+              type: "actions",
+              width: 150,
+              getActions: (params) => [
+                <GridActionsCellItem
+                  icon={<DownloadForOfflineIcon />}
+                  label="Descargar"
+                  onClick={() => descargar(params.row.id)}
+                />,
+                /* <GridActionsCellItem
                     icon={<ViewKanbanIcon />}
                     label="KanBan"
                     onClick={() => this.kanban(params.row.id)}
-                  />,
-                  <GridActionsCellItem
-                    icon={<EditIcon />}
-                    label="Editar"
-                    onClick={() =>
-                      this.editlink(params.row.id, params.row.modelo)
-                    }
-                  />,
-                  <GridActionsCellItem
-                    icon={<DeleteIcon />}
-                    label="Eliminar"
-                    onClick={() => this.eliminar(params.row.id)}
-                  />,
-                ],
-              },
-            ]}
-            pageSize={11}
-            rowsPerPageOptions={[11]}
-          />
-        </div>
-      </>
-    );
-  }
-}
+                  />, */
+                <GridActionsCellItem
+                  icon={<EditIcon />}
+                  label="Editar"
+                  onClick={() => editlink(params.row.id, params.row.modelo)}
+                />,
+                <GridActionsCellItem
+                  icon={<DeleteIcon />}
+                  label="Eliminar"
+                  onClick={() => eliminar(params.row.id)}
+                />,
+              ],
+            },
+          ]}
+          pageSize={11}
+          rowsPerPageOptions={[11]}
+        />
+      </div>
+    </>
+  );
+};
 
 export default TableProjects;
